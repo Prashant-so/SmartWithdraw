0bbf8a3742b67c98ea92e1aaae6d51c7136f3fe3
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-06 23:24:15 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,25 @@ repositories {
     mavenCentral()
 
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+    maven("https://repo.codemc.io/repository/maven-releases/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    archiveFileName.set("SmartWithdraw-${project.version}.jar")
 }
