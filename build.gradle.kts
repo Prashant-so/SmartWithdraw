c5c94f371907345cf6bbd7e7b5272a99fb296887
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-06 09:09:50 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+}
+
+group = "com.smartwithdraw"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
