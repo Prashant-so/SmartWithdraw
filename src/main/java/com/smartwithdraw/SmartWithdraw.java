4866e9c1acd62da492262ea892c29addaf604f3f
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-06 09:42:08 +0530

    Update SmartWithdraw.java

--- src/main/java/com/smartwithdraw/SmartWithdraw.java
+++ src/main/java/com/smartwithdraw/SmartWithdraw.java
@@ -1,12 +1,18 @@
 package com.smartwithdraw;
 
+import com.smartwithdraw.command.WithdrawCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SmartWithdraw extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         getLogger().info("SmartWithdraw enabled!");
+
+        if (getCommand("withdraw") != null) {
+            getCommand("withdraw").setExecutor(new WithdrawCommand());
+        }
     }
 
     @Override
