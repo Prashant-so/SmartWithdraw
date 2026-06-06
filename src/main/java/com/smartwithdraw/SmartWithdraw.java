6a7861bab9d4ed00e7372c98d1a5af7b4a6fa510
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-06 23:38:29 +0530

    Update SmartWithdraw.java

--- src/main/java/com/smartwithdraw/SmartWithdraw.java
+++ src/main/java/com/smartwithdraw/SmartWithdraw.java
@@ -1,22 +1,39 @@
 package com.smartwithdraw;
 
 import com.smartwithdraw.command.WithdrawCommand;
+import com.smartwithdraw.economy.EconomyManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SmartWithdraw extends JavaPlugin {
 
+    private static SmartWithdraw instance;
+
     @Override
     public void onEnable() {
 
-        getLogger().info("SmartWithdraw enabled!");
+        instance = this;
+
+        saveDefaultConfig();
+
+        if (!EconomyManager.setup(this)) {
+            getLogger().severe("Failed to hook into Vault!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
         if (getCommand("withdraw") != null) {
             getCommand("withdraw").setExecutor(new WithdrawCommand());
         }
+
+        getLogger().info("SmartWithdraw enabled successfully!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("SmartWithdraw disabled!");
+    }
+
+    public static SmartWithdraw getInstance() {
+        return instance;
     }
 }
