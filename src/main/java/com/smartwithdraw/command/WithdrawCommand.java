6a199484a1f9a0c7f294ab874e107ec4e7f90a7e
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-06 23:40:27 +0530

    Update WithdrawCommand.java

--- src/main/java/com/smartwithdraw/command/WithdrawCommand.java
+++ src/main/java/com/smartwithdraw/command/WithdrawCommand.java
@@ -1,32 +1,52 @@
 package com.smartwithdraw.command;
 
+import com.smartwithdraw.economy.EconomyManager;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class WithdrawCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (args.length != 1) {
-            sender.sendMessage("§cUsage: /withdraw <amount>");
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command.");
             return true;
         }
 
-        try {
-            int amount = Integer.parseInt(args[0]);
-
-            if (amount <= 0) {
-                sender.sendMessage("§cAmount must be greater than 0.");
-                return true;
-            }
-
-            sender.sendMessage("§aRequested withdrawal: §e₹" + amount);
-
-        } catch (NumberFormatException e) {
-            sender.sendMessage("§cPlease enter a valid number.");
+        if (args.length != 1) {
+            player.sendMessage(ChatColor.YELLOW + "Usage: /withdraw <amount>");
+            return true;
         }
+
+        double amount;
+
+        try {
+            amount = Double.parseDouble(args[0]);
+        } catch (NumberFormatException e) {
+            player.sendMessage(ChatColor.RED + "Please enter a valid number.");
+            return true;
+        }
+
+        if (amount <= 0) {
+            player.sendMessage(ChatColor.RED + "Amount must be greater than 0.");
+            return true;
+        }
+
+        if (EconomyManager.getBalance(player) < amount) {
+            player.sendMessage(ChatColor.RED + "You don't have enough money.");
+            return true;
+        }
+
+        if (!EconomyManager.withdraw(player, amount)) {
+            player.sendMessage(ChatColor.RED + "Transaction failed.");
+            return true;
+        }
+
+        player.sendMessage(ChatColor.GREEN + "Successfully withdrew ₹" + amount + ".");
 
         return true;
     }
