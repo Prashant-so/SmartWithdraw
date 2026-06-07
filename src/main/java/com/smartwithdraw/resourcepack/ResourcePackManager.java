b546730c16051c42b1da7fc539dff15bca6ed8a0
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 14:32:16 +0530

    Update ResourcePackManager.java

--- src/main/java/com/smartwithdraw/resourcepack/ResourcePackManager.java
+++ src/main/java/com/smartwithdraw/resourcepack/ResourcePackManager.java
@@ -2,13 +2,11 @@ package com.smartwithdraw.resourcepack;
 
 public final class ResourcePackManager {
 
-```
 private ResourcePackManager() {
 }
 
 public static boolean enabled() {
     return false;
 }
-```
 
 }
