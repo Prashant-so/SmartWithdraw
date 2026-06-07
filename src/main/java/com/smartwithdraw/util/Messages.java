90982c64793a1248cc560c40f2133f3fbba43591
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 10:22:29 +0530

    Update Messages.java

--- src/main/java/com/smartwithdraw/util/Messages.java
+++ src/main/java/com/smartwithdraw/util/Messages.java
@@ -8,4 +8,15 @@ public final class Messages {
     public static final String PREFIX =
             "§6§lSmartWithdraw §8» ";
 
+    public static final String NO_MONEY =
+            PREFIX + "§cYou do not have enough money.";
+
+    public static final String INVALID_AMOUNT =
+            PREFIX + "§cInvalid amount.";
+
+    public static final String WITHDRAW_SUCCESS =
+            PREFIX + "§aWithdraw successful.";
+
+    public static final String DEPOSIT_SUCCESS =
+            PREFIX + "§aDeposit successful.";
 }
