2e78bc75574dc6e2249de2232001359a276bd882
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 13:25:01 +0530

    Update WithdrawCommand.java

--- src/main/java/com/smartwithdraw/command/WithdrawCommand.java
+++ src/main/java/com/smartwithdraw/command/WithdrawCommand.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 public class WithdrawCommand implements CommandExecutor {
 
-```
+
 @Override
 public boolean onCommand(CommandSender sender,
                          Command command,
@@ -75,6 +75,6 @@ public boolean onCommand(CommandSender sender,
 
     return true;
 }
-```
+
 
 }
