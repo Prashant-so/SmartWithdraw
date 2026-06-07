088a259f10b27a7ed5c475db75c2fc103b3606d0
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 10:36:37 +0530

    Update SmartWithdraw.java

--- src/main/java/com/smartwithdraw/SmartWithdraw.java
+++ src/main/java/com/smartwithdraw/SmartWithdraw.java
@@ -3,42 +3,51 @@ package com.smartwithdraw;
 import com.smartwithdraw.command.WithdrawCommand;
 import com.smartwithdraw.economy.EconomyManager;
 import com.smartwithdraw.listener.NoteRedeemListener;
+import com.smartwithdraw.listener.ResourcePackListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SmartWithdraw extends JavaPlugin {
 
-    private static SmartWithdraw instance;
+```
+private static SmartWithdraw instance;
 
-    @Override
-    public void onEnable() {
+@Override
+public void onEnable() {
 
-        instance = this;
+    instance = this;
 
-        if (!EconomyManager.setupEconomy()) {
-            getLogger().severe("Vault not found!");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-
-        if (getCommand("withdraw") != null) {
-            getCommand("withdraw").setExecutor(new WithdrawCommand());
-        }
-
-        getServer().getPluginManager().registerEvents(
-                new NoteRedeemListener(),
-                this
-        );
-
-        saveDefaultConfig();
-
-        getLogger().info("SmartWithdraw enabled.");
+    if (!EconomyManager.setupEconomy()) {
+        getLogger().severe("Vault not found!");
+        getServer().getPluginManager().disablePlugin(this);
+        return;
     }
 
-    @Override
-    public void onDisable() {
+    if (getCommand("withdraw") != null) {
+        getCommand("withdraw").setExecutor(new WithdrawCommand());
     }
 
-    public static SmartWithdraw getInstance() {
-        return instance;
-    }
+    getServer().getPluginManager().registerEvents(
+            new NoteRedeemListener(),
+            this
+    );
+
+    getServer().getPluginManager().registerEvents(
+            new ResourcePackListener(),
+            this
+    );
+
+    saveDefaultConfig();
+
+    getLogger().info("SmartWithdraw enabled.");
+}
+
+@Override
+public void onDisable() {
+}
+
+public static SmartWithdraw getInstance() {
+    return instance;
+}
+```
+
 }
