113f9c149802d131ebcb563a892c1e2cdf806145
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 10:04:06 +0530

    Update NoteValidator.java

--- src/main/java/com/smartwithdraw/security/NoteValidator.java
+++ src/main/java/com/smartwithdraw/security/NoteValidator.java
@@ -1,1 +1,13 @@
+package com.smartwithdraw.security;
 
+import org.bukkit.inventory.ItemStack;
+
+public final class NoteValidator {
+
+    private NoteValidator() {
+    }
+
+    public static boolean isValid(ItemStack item) {
+        return item != null;
+    }
+}
