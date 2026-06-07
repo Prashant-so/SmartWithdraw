d7ca6ac2bd72cfce09750a921f143a192807636d
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 10:11:19 +0530

    Create Messages.java

---
+++ src/main/java/com/smartwithdraw/util/Messages.java
@@ -0,0 +1,11 @@
+package com.smartwithdraw.util;
+
+public final class Messages {
+
+    private Messages() {
+    }
+
+    public static final String PREFIX =
+            "§6§lSmartWithdraw §8» ";
+
+}
