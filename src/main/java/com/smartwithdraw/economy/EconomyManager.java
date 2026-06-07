621df4aa4b39eaab8e085ffc3971af27c77d1296
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 10:09:14 +0530

    Update EconomyManager.java

--- src/main/java/com/smartwithdraw/economy/EconomyManager.java
+++ src/main/java/com/smartwithdraw/economy/EconomyManager.java
@@ -2,6 +2,7 @@ package com.smartwithdraw.economy;
 
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public final class EconomyManager {
@@ -26,5 +27,17 @@ public final class EconomyManager {
 
     public static Economy getEconomy() {
         return economy;
+    }
+
+    public static boolean has(Player player, double amount) {
+        return economy.has(player, amount);
+    }
+
+    public static void withdraw(Player player, double amount) {
+        economy.withdrawPlayer(player, amount);
+    }
+
+    public static void deposit(Player player, double amount) {
+        economy.depositPlayer(player, amount);
     }
 }
