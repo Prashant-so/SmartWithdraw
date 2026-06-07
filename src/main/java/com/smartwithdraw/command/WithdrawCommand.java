7363c852d29f4b943122f9ce7513d98acefc30ae
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 10:12:08 +0530

    Update WithdrawCommand.java

--- src/main/java/com/smartwithdraw/command/WithdrawCommand.java
+++ src/main/java/com/smartwithdraw/command/WithdrawCommand.java
@@ -1,9 +1,11 @@
 package com.smartwithdraw.command;
 
 import com.smartwithdraw.currency.DenominationCalculator;
+import com.smartwithdraw.economy.EconomyManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.Map;
 
@@ -15,8 +17,13 @@ public class WithdrawCommand implements CommandExecutor {
                              String label,
                              String[] args) {
 
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Players only.");
+            return true;
+        }
+
         if (args.length != 1) {
-            sender.sendMessage("§cUsage: /withdraw <amount>");
+            player.sendMessage("§cUsage: /withdraw <amount>");
             return true;
         }
 
@@ -25,18 +32,23 @@ public class WithdrawCommand implements CommandExecutor {
             int amount = Integer.parseInt(args[0]);
 
             if (amount <= 0) {
-                sender.sendMessage("§cAmount must be greater than 0.");
+                player.sendMessage("§cAmount must be greater than 0.");
+                return true;
+            }
+
+            if (!EconomyManager.has(player, amount)) {
+                player.sendMessage("§cNot enough money.");
                 return true;
             }
 
             Map<Integer, Integer> notes =
                     DenominationCalculator.calculate(amount);
 
-            sender.sendMessage("§6§lSmartWithdraw §8» §aWithdrawal Breakdown");
+            player.sendMessage("§6§lSmartWithdraw §8» §aBreakdown:");
 
             for (Map.Entry<Integer, Integer> entry : notes.entrySet()) {
 
-                sender.sendMessage(
+                player.sendMessage(
                         "§e₹" + entry.getKey()
                                 + " §7x §f"
                                 + entry.getValue()
@@ -45,7 +57,7 @@ public class WithdrawCommand implements CommandExecutor {
 
         } catch (NumberFormatException ex) {
 
-            sender.sendMessage("§cPlease enter a valid number.");
+            player.sendMessage("§cPlease enter a valid number.");
 
         }
 
