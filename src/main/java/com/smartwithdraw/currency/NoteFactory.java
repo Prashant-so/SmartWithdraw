8a003aacca7950f4364e50be38f93fe24c81d04e
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-06-07 10:03:36 +0530

    Update NoteFactory.java

--- src/main/java/com/smartwithdraw/currency/NoteFactory.java
+++ src/main/java/com/smartwithdraw/currency/NoteFactory.java
@@ -1,1 +1,15 @@
+package com.smartwithdraw.currency;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public final class NoteFactory {
+
+    private NoteFactory() {
+    }
+
+    public static ItemStack createNote(int value) {
+
+        return new ItemStack(Material.PAPER);
+    }
+}
