d1a780d920937d209eb8c5e46485750dea794669
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-07-12 18:02:52 +0530

    Update SmartWithdraw.java

--- src/main/java/com/smartwithdraw/SmartWithdraw.java
+++ src/main/java/com/smartwithdraw/SmartWithdraw.java
@@ -3,9 +3,15 @@ package com.smartwithdraw;
 import com.smartwithdraw.command.DepositCommand;
 import com.smartwithdraw.command.SmartWithdrawCommand;
 import com.smartwithdraw.command.WithdrawCommand;
+import com.smartwithdraw.currency.CurrencyManager;
 import com.smartwithdraw.economy.EconomyManager;
+import com.smartwithdraw.listener.BankMenuListener;
 import com.smartwithdraw.listener.NoteRedeemListener;
+import com.smartwithdraw.listener.NoteSplitListener;
 import com.smartwithdraw.listener.ResourcePackListener;
+import com.smartwithdraw.placeholder.SmartWithdrawExpansion;
+import com.smartwithdraw.security.SecretKeyManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SmartWithdraw extends JavaPlugin {
@@ -17,11 +23,16 @@ public final class SmartWithdraw extends JavaPlugin {
 
         instance = this;
 
+        saveDefaultConfig();
+
         if (!EconomyManager.setupEconomy()) {
             getLogger().severe("Vault not found!");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
+
+        SecretKeyManager.load();
+        CurrencyManager.load();
 
         if (getCommand("withdraw") != null) {
             getCommand("withdraw").setExecutor(new WithdrawCommand());
@@ -35,17 +46,19 @@ public final class SmartWithdraw extends JavaPlugin {
             getCommand("smartwithdraw").setExecutor(new SmartWithdrawCommand());
         }
 
-        getServer().getPluginManager().registerEvents(
-                new NoteRedeemListener(),
-                this
-        );
+        getServer().getPluginManager().registerEvents(new NoteRedeemListener(), this);
+        getServer().getPluginManager().registerEvents(new NoteSplitListener(), this);
+        getServer().getPluginManager().registerEvents(new BankMenuListener(), this);
+        getServer().getPluginManager().registerEvents(new ResourcePackListener(), this);
 
-        getServer().getPluginManager().registerEvents(
-                new ResourcePackListener(),
-                this
-        );
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new SmartWithdrawExpansion().register();
+            getLogger().info("Hooked into PlaceholderAPI.");
+        }
 
-        saveDefaultConfig();
+        if (Bukkit.getPluginManager().getPlugin("PlayerPoints") != null) {
+            getLogger().info("Hooked into PlayerPoints.");
+        }
 
         getLogger().info("SmartWithdraw enabled.");
     }
