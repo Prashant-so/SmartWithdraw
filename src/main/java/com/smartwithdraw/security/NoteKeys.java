c5bba8ee53f3c73ec525e0a2fa14873319566315
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-08-09 07:24:16 +0530

    Update NoteKeys.java

--- src/main/java/com/smartwithdraw/security/NoteKeys.java
+++ src/main/java/com/smartwithdraw/security/NoteKeys.java
@@ -9,5 +9,5 @@ public final class NoteKeys {
     public static final String NOTE_MARKER    = "smartwithdraw_note";
     public static final String NOTE_SIGNATURE = "note_signature";
     public static final String NOTE_CURRENCY  = "note_currency";
-    public static final String NOTE_CREATED   = "note_created";   // epoch ms, for expiry
+    public static final String NOTE_CREATED   = "note_created";
 }
