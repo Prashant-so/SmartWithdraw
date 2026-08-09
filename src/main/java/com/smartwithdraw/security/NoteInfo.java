fb8f333560d7b77dc046824cee0b7131e0705838
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-08-09 07:26:19 +0530

    Update NoteInfo.java

--- src/main/java/com/smartwithdraw/security/NoteInfo.java
+++ src/main/java/com/smartwithdraw/security/NoteInfo.java
@@ -5,6 +5,6 @@ import com.smartwithdraw.currency.Currency;
 public record NoteInfo(
         Currency currency,
         int value,
-        long createdAt  // epoch ms when the note was created
+        long createdAt
 ) {
 }
